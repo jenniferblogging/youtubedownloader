972d696d58bc09e6cf18f4eff92d797dea84e14c
Jennifer <ashishfake1234@gmail.com>
2023-03-15 06:42:08 +0530

    Deployed website

--- src/Mycomponants/AddTodo.js
+++ src/Mycomponants/AddTodo.js
@@ -7,6 +7,7 @@ export default function AddTodo({todos,submit}) {
   let Sno;
   let todo;
 
+  
   if(todos.length===0){
     Sno=1;
   }
@@ -36,15 +37,40 @@ export default function AddTodo({todos,submit}) {
     </form>
         </div>*/}
  <div className='container'>
+     {/*
       <form className='formContainer my-3' onSubmit={(e)=>{submit(e,todo);
       //setHeading('');
       setDescription(todo.Description);
       }}>
   <div className="form-group my-3 text-center">
-    <label htmlFor="textInputHeading"><h1>Youtube Downloader!!!!</h1></label>
+    <label htmlFor="textInputHeading"><h1>Youtube Downloader</h1></label>
     <input type="text" className="form-control my-3 form-control-lg" value={heading} placeholder="Enter Youtube URL here" onChange={(e) => setHeading(e.target.value)}/>
     <button type="submit" className="btn btn-primary my-3 btn-lg">Submit</button>
   </div>
+
+    </form>*/}
+     <form className='formContainer my-3' onSubmit={(e)=>{submit(e,todo);
+      //setHeading('');
+      setDescription(todo.Description);
+      }}>
+        
+  <section className="home-newsletter">
+<div className="container">
+<div className="row">
+<div className="col-sm-12">
+	<div className="single">
+		<h2>Youtube Downloader</h2>
+	<div className="input-group">
+         <input type="text" className="form-control" value={heading} placeholder="Enter Youtube URL here" onChange={(e) => setHeading(e.target.value)}/>
+         <span className="input-group-btn">
+         <button className="btn btn-theme" type="submit">Search</button>
+         </span>
+          </div>
+	</div>
+</div>
+</div>
+</div>
+</section>
 
     </form>
         </div>
